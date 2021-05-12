dace819d8f2eca304ca3edbf30393355610c97a8
MeshalAlamr <68873733+MeshalAlamr@users.noreply.github.com>
2021-05-12 06:40:26 +0300

    Update visualize.m

--- visualize/visualize.m
+++ visualize/visualize.m
@@ -7,7 +7,7 @@ close all
 data = readNPY('val_data.npy');
 data_ds = readNPY('val_data_downsampled.npy'); %downsampled
 
-n = 10; %video number
+n = 10; % video number
 k = 1;
 for i = [1:300]
     
@@ -25,10 +25,8 @@ for i = [1:300]
 
     scatter3(ax1,j(1,1,1,1:25),j(1,2,1,1:25),j(1,3,1,1:25))
     hold(ax1,'on')
-    %     subplot(1,2,2)
     scatter3(ax2,j_ds(1,1,1,1:25),j_ds(1,2,1,1:25),j_ds(1,3,1,1:25))
     hold(ax2,'on')
-    %     axis equal
     pts = [];
     pts_ds = [];
     for j = [1:25]
@@ -50,11 +48,9 @@ for i = [1:300]
         i = i+1;
     end
     
-    %     subplot(1,2,1)
     line(ax1,adj1(:,1), adj1(:,2),adj1(:,3))
     plot3(ax1,adj1(:,1), adj1(:,2),adj1(:,3))
     hold(ax1,'on')
-    %     subplot(1,2,2)
     line(ax2,adj1_ds(:,1), adj1_ds(:,2),adj1_ds(:,3))
     plot3(ax2,adj1_ds(:,1), adj1_ds(:,2),adj1_ds(:,3))
     hold(ax2,'on')
@@ -71,11 +67,11 @@ for i = [1:300]
         
         
     end
-    %     subplot(1,2,1)
+
     line(ax1,adj2(:,1), adj2(:,2),adj2(:,3))
     plot3(ax1,adj2(:,1), adj2(:,2),adj2(:,3))
     hold(ax1,'on')
-    %     subplot(1,2,2)
+
     line(ax2,adj2_ds(:,1), adj2_ds(:,2),adj2_ds(:,3))
     plot3(ax2,adj2_ds(:,1), adj2_ds(:,2),adj2_ds(:,3))
     hold(ax2,'on')
@@ -91,33 +87,22 @@ for i = [1:300]
         i = i+1;
         
     end
-    %     subplot(1,2,1)
+
     line(ax1,adj3(:,1), adj3(:,2),adj3(:,3))
     plot3(ax1,adj3(:,1), adj3(:,2),adj3(:,3))
     hold(ax1,'on')
-    %     subplot(1,2,2)
+
     line(ax2,adj3_ds(:,1), adj3_ds(:,2),adj3_ds(:,3))
     plot3(ax2,adj3_ds(:,1), adj3_ds(:,2),adj3_ds(:,3))
     hold(ax2,'on')
     
     title(ax1,'Normal')
     title(ax2,'Downsampled')
-    ylim(ax1,[-1.8 0.8])
-    ylim(ax2,[-1.8 0.8])
-    xlim(ax1,[-0.8 0])
-    xlim(ax2,[-0.8 0])
-    zlim(ax1,[3.2 3.8])
-    zlim(ax2,[3.2 3.8])
-   
-%     ylim(ax1,[-1.4 1.2])
-%     ylim(ax2,[-1.4 1.2])
-%     xlim(ax1,[-1.3 1.5])
-%     xlim(ax2,[-1.3 1.5])
-%     zlim(ax1,[-0.5 4.6])
-%     zlim(ax2,[-0.5 4.6])
     
+    # 2D-View
     view(ax1,0,90)
     view(ax2,0,90)
+    
     pause(0.03)
     
     k = k+1;
